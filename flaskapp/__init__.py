674a33445068c7cd83edcff8679d48c35ef150d6
liukai <liukaiv5@gmail.com>
2020-07-26 15:25:13 +0800

    fix up

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -5,8 +5,8 @@ import logging
 # set logger
 logging.basicConfig(filename='C:\\Temp\\flask-service.log', level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 # create app
 app = Flask(__name__)
+
 # create api
 api = Api(app)
--- run.py
+++ run.py
@@ -1,56 +1,12 @@
 from flaskapp import app, views
 import inspect
 import os
-import sys
 import json
 
-# port = custom_config.get('port', 5002)
-# host = custom_config.get('host', '127.0.0.1')
-
-app.logger.info('relative_path:{}'.format(os.path.abspath('.')))
-
-app.logger.info(
-    'inspect path:{}'.format(
-        os.path.dirname(inspect.currentframe().f_globals.get('__file__'))))
-app.logger.info('_MEIPASS env:{}'.format(os.environ.get("_MEIPASS2", 'None')))
-try:
-    pyinstaller_path = sys._MEIPASS
-except:
-    pyinstaller_path = os.environ.get("_MEIPASS2", 'None')
-app.logger.info('pyinstaller path:{}'.format(pyinstaller_path))
-
-# load custom config
-# def resource_path(relative_path):
-#     """ Get absolute path to resource, works for dev and for PyInstaller """
-#     try:
-#         # PyInstaller creates a temp folder and stores path in _MEIPASS
-#         base_path = sys._MEIPASS
-#         app.logger.info(base_path)
-#         app.logger.info(os.path.abspath('.'))
-#     except Exception:
-#         base_path = os.environ.get("_MEIPASS2", os.path.abspath("."))
-#     return os.path.join(base_path, relative_path)
-#
-#
-# custom_config_file = resource_path('custom_config.json')
-# if os.path.isfile(custom_config_file):
-#     app.logger.info(
-#         'load custom config from file {}'.format(custom_config_file))
-#     custom_config = json.load(open(resource_path('custom_config.json')))
-# else:
-#     app.logger.error(
-#         'custom config file {} not found'.format(custom_config_file))
-#     import inspect
-#
-#     path = inspect.currentframe().f_globals.get('__path__')
-#     app.logger.info('inspect path {}'.format(path))
-#     app.logger.info('sys argv {}'.format(str(sys.argv)))
-#
-#     app.logger.info('sys excutable {}'.format(str(sys.executable)))
-#     app.logger.info('sys keys {}'.format(str(sys.__dict__.keys())))
-#     custom_config = {}
-
-custom_config = {}
+work_folder = os.path.dirname(inspect.currentframe().f_globals.get('__file__'))
+app.logger.info('loading custom config from {}'.format(work_folder))
+with open(os.path.join(work_folder, 'custom_config.json'), 'r') as fp:
+    custom_config = json.load(fp)
 
 
 def main():
